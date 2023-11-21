06e7d6c541483237bb3db6e774b45190d39f6e89
PorterRT <91439082+PorterRT@users.noreply.github.com>
2023-11-21 00:20:00 +0000

    version 0.3

--- blackJack.py
+++ blackJack.py
@@ -4,19 +4,19 @@ import supervision as sv
 import openai
 
 # Replace with your OpenAI API key
-openai.api_key = 'OPENAI_API_KEY'
+openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-# Function to read goals/instructions from a text file
-def read_goals_from_file(file_path):
+# Function to read instructions/goals from a text file
+def read_instructions_from_file(file_path):
     with open(file_path, 'r') as file:
-        goals = file.read()
-    return goals
+        instructions = file.read()
+    return instructions
 
-# Create an OpenAI GPT-4 conversation with the goals
-def create_gpt4_conversation(goals):
+# Create an OpenAI GPT-4 conversation with the instructions
+def create_gpt4_conversation(instructions):
     conversation = [
         {"role": "system", "content": "You are a robot that can interact with the webcam."},
-        {"role": "user", "content": goals},
+        {"role": "user", "content": instructions},
     ]
     return conversation
 
@@ -34,11 +34,11 @@ def on_prediction(predictions, image):
     detections = detections[detections.confidence > 0.45]
     print(detections)
     
-    # Read goals from the text file
-    goals = read_goals_from_file("goals.txt")
+    # Read instructions from the text file "Player.txt"
+    instructions = read_instructions_from_file("Player.txt")
     
-    # Create a GPT-4-turbo conversation with the goals
-    conversation = create_gpt4_conversation(goals)
+    # Create a GPT-4-turbo conversation with the instructions
+    conversation = create_gpt4_conversation(instructions)
     
     # Use GPT-4-turbo to get a response
     gpt4_response = openai.ChatCompletion.create(
@@ -46,7 +46,7 @@ def on_prediction(predictions, image):
         messages=conversation
     )
     
-    # Handle the GPT-3 response
+    # Handle the GPT-4 response
     handle_gpt4_response(gpt4_response)
     
     # Display the annotated image
